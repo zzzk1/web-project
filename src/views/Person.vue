a3a5c11d04720139382dfe5d8bbfae13ca74c908
zzzk1 <36791902+zzzk1@users.noreply.github.com>
2023-04-24 18:56:04 +0800

    个人头像

--- src/views/Person.vue
+++ src/views/Person.vue
@@ -1,6 +1,15 @@
 <template>
     <el-card style="width: 500px;">
         <el-form label-width="80px" size="small">
+            <el-upload
+                class="avatar-uploader"
+                action="http://localhost:9090/file/upload"
+                :show-file-list="false"
+                :on-success="handleAvatarSuccess"
+            >
+                <img v-if="form.avatarUrl" :src="form.avatarUrl" class="avatar">
+                <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+            </el-upload>
             <el-form-item label="用户名">
                 <el-input v-model="form.username" autocomplete="off"></el-input>
             </el-form-item>
@@ -44,20 +53,57 @@ export default {
         })
     },
     methods: {
+        async getUser() {
+            return (await request.get("/user/username/" + this.user.token)).data
+        },
         save() {
             request.post("/user", this.form).then(res => {
                 if (res) {
+                    // 更新浏览器存储的用户信息
+                    this.getUser().then(res => {
+                        res.token = JSON.parse(localStorage.getItem("user")).token
+                        localStorage.setItem("user", JSON.stringify(res))
+                    })
                     this.$message.success("保存成功")
                 } else {
                     this.$message.error("保存失败")
                 }
             })
         },
+        handleAvatarSuccess(res) {
+            this.form.avatarUrl = res
+        }
     }
 
 }
 </script>
 
 <style scoped>
-
+.avatar-uploader {
+    text-align: center;
+    padding-bottom: 10px;
+}
+.avatar-uploader .el-upload {
+    border: 1px dashed #d9d9d9;
+    border-radius: 6px;
+    cursor: pointer;
+    position: relative;
+    overflow: hidden;
+}
+.avatar-uploader .el-upload:hover {
+    border-color: #409EFF;
+}
+.avatar-uploader-icon {
+    font-size: 28px;
+    color: #8c939d;
+    width: 138px;
+    height: 138px;
+    line-height: 138px;
+    text-align: center;
+}
+.avatar {
+    width: 138px;
+    height: 138px;
+    display: block;
+}
 </style>
