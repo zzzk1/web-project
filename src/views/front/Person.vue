a2063a1bdfc53b7fa1c8fdae0c1d137afd3b0e37
zzzk1 <36791902+zzzk1@users.noreply.github.com>
2023-04-26 14:33:16 +0800

    前台个人信息

--- src/views/front/Person.vue
+++ src/views/front/Person.vue
@@ -1,6 +1,15 @@
 <template>
     <el-card style="width: 500px;">
         <el-form label-width="80px" size="small">
+            <el-upload
+                class="avatar-uploader"
+                action="http://localhost:9090/file/upload"
+                :show-file-list="false"
+                :on-success="handleAvatarSuccess"
+            >
+                <img v-if="form.avatarUrl" :src="form.avatarUrl" class="avatar">
+                <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+            </el-upload>
             <el-form-item label="用户名">
                 <el-input v-model="form.username" autocomplete="off"></el-input>
             </el-form-item>
@@ -33,31 +42,72 @@ export default {
         return {
             form: {},
             user: localStorage.getItem("user") ? JSON.parse(localStorage.getItem("user")) : {}
-
         }
     },
     created() {
-        request.get("/user/username/" + this.user.token).then(res => {
-            if (res.code == '200') {
-                this.form = res.data
-            }
+        this.getUser().then(res => {
+            console.log(res)
+            this.form = res
         })
     },
     methods: {
+        async getUser() {
+            return (await request.get("/user/username/" + this.user.username)).data
+        },
         save() {
             request.post("/user", this.form).then(res => {
-                if (res) {
+                if (res.code == '200') {
                     this.$message.success("保存成功")
+
+                    // 触发父级更新User的方法
+                    this.$emit("refreshUser")
+
+                    // 更新浏览器存储的用户信息
+                    this.getUser().then(res => {
+                        res.token = JSON.parse(localStorage.getItem("user")).token
+                        localStorage.setItem("user", JSON.stringify(res))
+                    })
+
                 } else {
                     this.$message.error("保存失败")
                 }
             })
         },
+        handleAvatarSuccess(res) {
+            console.log(res)
+            this.form.avatarUrl = res.data
+        }
     }
 
 }
 </script>
 
 <style scoped>
-
+.avatar-uploader {
+    text-align: center;
+    padding-bottom: 10px;
+}
+.avatar-uploader .el-upload {
+    border: 1px dashed #d9d9d9;
+    border-radius: 6px;
+    cursor: pointer;
+    position: relative;
+    overflow: hidden;
+}
+.avatar-uploader .el-upload:hover {
+    border-color: #409EFF;
+}
+.avatar-uploader-icon {
+    font-size: 28px;
+    color: #8c939d;
+    width: 138px;
+    height: 138px;
+    line-height: 138px;
+    text-align: center;
+}
+.avatar {
+    width: 138px;
+    height: 138px;
+    display: block;
+}
 </style>
